7add14f3ae070a8ec564d8abe1cc8a429ff21072
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2025-11-11 01:24:54 +0100

    savepoint

--- examples/python-uv-3.13/entrypoint.sh
+++ examples/python-uv-3.13/entrypoint.sh
@@ -4,5 +4,10 @@ set -e
 # This script allows flexible argument passing to pytest
 # It ensures the virtual environment is activated properly
 
-# Execute pytest with all arguments passed to this script
-exec pytest "$@"
+# If no arguments provided, run pytest with default args
+# Otherwise, execute the provided command directly
+if [ $# -eq 0 ]; then
+  exec pytest
+else
+  exec "$@"
+fi
